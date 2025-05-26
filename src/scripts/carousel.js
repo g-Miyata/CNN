b7db6d6196b0156504cf2278dea11a7c16f2177d
Guilherme Miyata <miyatagui120600@gmail.com>
2025-05-26 17:07:45 -0300

    chore: ajuste de navegação no carroussel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,5 +1,5 @@
 function scrollGames(direction) {
   const container = document.querySelector('.games');
-  const scrollAmount = 1272;
+  const scrollAmount = 305;
   container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
 }
